d6ef68e6615843dbb01702292ab59eff0acc86c0
Jonathas dos Santos <89552550+Johnnywbs@users.noreply.github.com>
2023-11-23 15:01:47 -0300

    Update urls.py

--- Baque/judge/urls.py
+++ Baque/judge/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("",views.home,name="home"),
+    path("criar_questao/",views.criar_questao,name="criar_questao"),
+    path("questao/",views.questao,name="questao")
 ]
