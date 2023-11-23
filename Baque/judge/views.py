74df8dcc6dd3e03babe2abd132c37a3a9d66e40f
Jonathas dos Santos <89552550+Johnnywbs@users.noreply.github.com>
2023-11-23 15:01:23 -0300

    Update views.py

--- Baque/judge/views.py
+++ Baque/judge/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World!")
+def home(request):
+    return render(request,"",)
+
+def criar_questao(request):
+    '''if request.method == 'POST':
+        # …
+    else:'''
+    return render(request,"",)
+
+def questao(request):
+    return render(request,"",)
